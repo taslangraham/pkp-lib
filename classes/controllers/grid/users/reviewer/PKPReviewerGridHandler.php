21821d5dbac38568014ae8e7be95a15dbf9298cc
Taslan Graham <gtaslan1@gmail.com>
2024-11-18 11:53:48 -0500

    Code cleanup

--- classes/controllers/grid/users/reviewer/PKPReviewerGridHandler.php
+++ classes/controllers/grid/users/reviewer/PKPReviewerGridHandler.php
@@ -1054,7 +1054,7 @@ class PKPReviewerGridHandler extends GridHandler
         };
         $template = Repo::emailTemplate()->getByKey($context->getId(), $request->getUserVar('template'));
 
-        if (!$template) {
+        if (!$template || ! Repo::emailTemplate()->isTemplateAccessibleToUser($request->getUser(), $template, $context->getId())) {
             return null;
         }
 
